d6372b985afa4685d13a93fea442761b4334e06b
Tyler Garder <tylerg@USA-IT-TYLERG.local>
2015-05-06 18:49:42 -0600

    final code

--- js/itunesService.js
+++ js/itunesService.js
@@ -1,7 +1,40 @@
 var app = angular.module('itunes');
 
 app.service('itunesService', function($http, $q){
-  //This service is what will do the 'heavy lifting' and get our data from the iTunes API.
+ 
+	 this.getArtist = function(artist){
+	 	var deferred = $q.defer();
+	 		$http({
+	 		method: 'JSONP',
+	 		url: 'https://itunes.apple.com/search?term=' + artist + '&callback=JSON_CALLBACK'
+	 	}).then(function(response){
+	 		response = loopResult(response.data.results);
+	 		deferred.resolve(response);
+	 	})
+	 	return  deferred.promise
+	  };
+
+
+	
+		var loopResult = function (data) {
+			console.log(data)
+			newArr = [];
+		    for(var i = 0; i < data.length; i++){
+			    var artistObj = {}; 
+			      artistObj.AlbumArt = data[i].artworkUrl100,
+			      artistObj.Artist = data[i].artistName,
+			      artistObj.Collection = data[i].collectionName,
+			      artistObj.CollectionPrice = data[i].CollectionPrice,
+			      artistObj.Play = data[i].previewURL,
+			      artistObj.Type = data[i].kind
+			      newArr.push(artistObj);
+			    };
+		    return newArr;
+		};
+
+});
+
+ //This service is what will do the 'heavy lifting' and get our data from the iTunes API.
   //Also not that we're using a 'service' and not a 'factory' so all your method you want to call in your controller need to be on 'this'.
 
   //Write a method that accepts an artist's name as the parameter, then makes a 'JSONP' http request to a url that looks like this
@@ -10,4 +43,3 @@ app.service('itunesService', function($http, $q){
   //You can return the http request or you can make your own promise in order to manipulate the data before you resolve it.
 
     //Code here
-});
--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -22,15 +22,18 @@ app.controller('mainCtrl', function($scope, itunesService){
 
   //First inject itunesService into your controller.
 
-    //code here
+
 
 
   //Now write a function that will call the method on the itunesService that is responsible for getting the data from iTunes, whenever the user clicks the submit button
   //*remember, that method should be expecting an artist name. The artist name is coming from the input box on index.html, head over there and check if that input box is tied to any specific model we could use.
   //Also note that that method should be retuning a promise, so you could use .then in this function.
     
-    //Code here
-
+    $scope.getSongData = function(){
+      itunesService.getArtist($scope.artist).then(function(data){
+      $scope.songData = data;
+    })
+  };
 
   //Check that the above method is working by entering a name into the input field on your web app, and then console.log the result
 
@@ -49,12 +52,12 @@ app.controller('mainCtrl', function($scope, itunesService){
   */
   //the iTunes API is going to give you a lot more details than ng-grid wants. Create a new array and then loop through the iTunes data pushing into your new array objects that look like the above data.
 
-    //Code here
 
 
   //Once you have that final data array, you simply need to put it on the scope (or more specifically on the scope as songData). Once you do this ($scope.songData = myFinalArray) then ng-grid will see that and populate the page.
 
     //Code here
+
 });
 
 
